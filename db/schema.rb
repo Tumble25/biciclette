b32cdd7e764a8349034ac307e8f6fe5b8d07d0c9
Rob Tuinte <robtuinte15@gmail.com>
2020-10-03 10:06:52 +0200

    added product category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_132438) do
+ActiveRecord::Schema.define(version: 2020_10_03_080615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "naam"
